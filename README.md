44ca092c2495bff17b985f72e79644e3a204249c
lmoesle <lukas95m@yahoo.de>
2021-02-09 11:03:34 +0100

    initial

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # kalenderwoche
+
+## Install
+
+TODO
+
+## Usage
+
+```
+kalenderwoche.py
+
+Usage:
+    ./kalenderwoche.py
+    ./kalenderwoche.py --date <date>
+    ./kalenderwoche.py -h|--help
+Options:
+    <date> An optional date (e.g. 2021-01-01)
+    -h --help  Show this screen.
+```
---
+++ kalenderwoche.py
@@ -0,0 +1,21 @@
+"""kalenderwoche.py
+Usage:
+    ./kalenderwoche.py
+    ./kalenderwoche.py --date <date>
+    ./kalenderwoche.py -h|--help
+Options:
+    <date> An optional date (e.g. 2021-01-01)
+    -h --help  Show this screen.
+"""
+import datetime
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    datum = datetime.datetime.now()
+    if arguments['--date']:
+        datum = datetime.datetime.strptime(arguments['<date>'], '%Y-%m-%d')
+    kalenderwoche = datum.isocalendar()[1]
+
+    print(f'{datum.strftime("%d.%m")} liegt in Kalenderwoche {kalenderwoche} im Jahr {datum.strftime("%Y")}')
